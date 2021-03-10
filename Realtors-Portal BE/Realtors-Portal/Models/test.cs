0045406e8e84ac99a8eb311081961bb8485d4715
DucNhu <60964371+DucNhu@users.noreply.github.com>
2021-03-10 19:16:41 +0700

    delete file

---
+++ Realtors-Portal BE/Realtors-Portal/Models/test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Realtors_Portal.Models
+{
+    public class test
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
