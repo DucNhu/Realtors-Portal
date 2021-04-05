9e07f8bacab4b903d31c994f038b1d388608c397
DESKTOP-AANKQLP\THINH <ncthinh.199@gmail.com>
2021-04-05 10:20:42 +0700

    commit by Thinh

---
+++ Realtors-Portal BE/Realtors-Portal/Migrations/20210405020645_thinh.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Realtors_Portal.Migrations
+{
+    public partial class thinh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
